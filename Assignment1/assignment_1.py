bd3550dc9d48e68cacf1658cc9f6d7b855a995be
MQuaresma <miguelquaresma97@gmail.com>
2018-10-19 11:55:16 +0100

    Documentation and performance tweaks

--- Assignment1/assignment_1.py
+++ Assignment1/assignment_1.py
@@ -1,24 +1,14 @@
 #!/usr/bin/env python3
+#coding=utf8
 
-# Assignment 1 - Remove Accentuation
+""" Convert text to ASCII.
 
-import re
+Write a program that reads unicode text as input and converts
+it to ASCII text, removing accents and other non-ASCII characters,
+keep the case.
+"""
 import sys
-
-# List of possible accentuations for each vogal
-a_accent = r'[àáãâÀÁÃÂ]'
-e_accent = r'[éèêÉÈÊ]'
-i_accent = r'[íìîÍÌÎ]'
-o_accent = r'[óòôõÓÒÔÕ]'
-u_accent = r'[úùûÚÙÛ]'
-
-replacement = {
-		a_accent:'aA',
-		e_accent:'eE',
-		i_accent:'iI',
-		o_accent:'oO',
-		u_accent:'uU'	
-	}
+import re
 
 def remove_accents(text):
 	res=""
@@ -34,11 +24,31 @@ def remove_accents(text):
 		res+=char
 	return res
 
-if(len(sys.argv)<=1):
-	inp = input();
-	print(remove_accents(inp))
-else:
-	for file in sys.argv[1:]:
-		f = open(file, 'r')
-		print("------------ " + file + " ------------ ")
-		print(remove_accents(f.read()))
+if __name__=="__main__":
+	a_accent = r'[àáãâÀÁÃÂ]'
+	e_accent = r'[éèêÉÈÊ]'
+	i_accent = r'[íìîÍÌÎ]'
+	o_accent = r'[óòôõÓÒÔÕ]'
+	u_accent = r'[úùûÚÙÛ]'
+
+	replacement = {
+		a_accent:'aA',
+		e_accent:'eE',
+		i_accent:'iI',
+		o_accent:'oO',
+		u_accent:'uU'	
+	}
+
+	if(len(sys.argv)<=1):
+		inp = input();
+		print(remove_accents(inp))
+	else:
+		for file in sys.argv[1:]:
+			f = open(file, 'r')
+			print("------------ " + file + " ------------ ")
+			line = f.readline()
+			while(len(line) > 0):
+				print(remove_accents(line))
+				line=f.readline()
+
+__author__ = "Joao Vieira and Miguel Quaresma"
