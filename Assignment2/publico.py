3562af8c08b6b4ec6ab5e1783730434a76e4dd65
JoaoVieira97 <jonasv55@hotmail.com>
2018-11-18 12:07:48 +0000

    Beginning to write in a latex file, some changes on code

--- Assignment2/publico.py
+++ Assignment2/publico.py
@@ -2,9 +2,15 @@ import requests
 import re
 import sys
 import html
+from urllib.request import urlretrieve
 
-def limparParagrafo(paragrafo):
-	return html.unescape(paragrafo)
+file = open("noticias.tex", "w")
+img_nb = 1
+
+def limparTexto(texto):
+	t = re.sub(r'<a href=".*".*>(.*)</a>', r'\1', texto)
+	t = re.sub(r'<.*>', ' ', t)
+	return html.unescape(t)
 
 def obterNoticias(links):
 	for link in links:
@@ -19,25 +25,39 @@ def obterNoticias(links):
 		paragrafos = paragrafos + re.findall(r'<p>\s*(.*)\s*</p>.*\s*.*<aside class=".*">', link_content.text)
 
 		print(titulo[0] + '\n')
-		print(description[0] + '\n')
+		print(limparTexto(description[0]) + '\n')
 		for image in images:
 			print(image)
 		for paragrafo in paragrafos:
-			print(limparParagrafo(paragrafo))
+			print(limparTexto(paragrafo))
 		print("\n---------------------------------\n")
+		escreverLatex(titulo[0], description[0], images, paragrafos)
+
+def iniciarLatex():
+	file.write("\\documentclass{article}\n")
+	file.write("\\usepackage[a4paper, top=3cm, left=3cm, right=2.5cm, bottom=2.5cm]{geometry}\n")
+	file.write("\\usepackage[utf8]{inputenc}\n")
+	file.write("\\usepackage{graphicx}\n\n")
+	file.write("\\begin{document}\n\n")
+
+def escreverLatex(titulo, description, images, paragrafos):
+	file.write("\\section{" + titulo + "}\n\n")
+	file.write("\\textbf{" + description + "}\n\n")
+	#for image in images:
+		#GUARDAR IMAGEM E SEU PATH, ESCREVER NO LATEX
+	for paragrafo in paragrafos:
+		file.write(limparTexto(paragrafo))
 
 def pesquisarNoticias():
 	for argument in sys.argv[1:]:
 		print("Termo de pesquisa: " + argument)
-		
 		payload = {'query':argument}
-		
 		r = requests.get('https://www.publico.pt/pesquisa', params = payload)
 		print('URL = ' + r.url)
-
 		links = re.findall(r'<div class="media-object-section">\s*<a href="(.*)">', r.text)
 		#print(links)
-
 		obterNoticias(links)
 
+iniciarLatex()
 pesquisarNoticias()
+file.write("\n\n\\end{document}\n")
