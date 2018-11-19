10b284473ef2a2effef28c05aa14776e53d07cd8
JoaoVieira97 <jonasv55@hotmail.com>
2018-11-19 13:35:42 +0000

    Changes on code, creating now '.files' folder to images and other changes. Latex seems to work fine

--- Assignment2/latex_utils.py
+++ Assignment2/latex_utils.py
@@ -1,3 +1,6 @@
+import re
+import html
+
 def iniciarLatex(f_descriptor):
     f_descriptor.write("\\documentclass{article}\n")
     f_descriptor.write("\\usepackage[a4paper, top=3cm, left=3cm, right=2.5cm, bottom=2.5cm]{geometry}\n")
@@ -8,14 +11,20 @@ def iniciarLatex(f_descriptor):
 
 def escreverLatex(out, titulo, description, images, paragrafos):
     out.write("\\section{" + titulo + "}\n\n")
-    out.write("\\textbf{" + description + "}\n\n")
+    out.write("\\textbf{" + limparTexto(description) + "}\n\n")
     for image in images:
        adicionar_img(out, image) 
     for paragrafo in paragrafos:
-        out.write(paragrafo)
+        out.write(limparTexto(paragrafo))
+    out.write("\n\\newpage\n")
 
 def adicionar_img(out, img_path):
     out.write('\\begin{figure}[H]\n')
     out.write('\t\\centering\n')
-    out.write('\t\\includegraphics{'+img_path+'}\n')
-    out.write('\\end{figure}')
+    out.write('\t\\includegraphics[width=0.3\\textwidth]{'+img_path+'}\n')
+    out.write('\\end{figure}\n\n')
+
+def limparTexto(texto):
+    t = re.sub(r'<a href=".*".*>(.*)</a>', r'\1', texto)
+    t = re.sub(r'<.*>', ' ', t)
+    return html.unescape(t)
--- Assignment2/publico.py
+++ Assignment2/publico.py
@@ -5,18 +5,14 @@ import os
 import html
 import latex_utils
 from urllib.request import urlretrieve
-
-def limparTexto(texto):
-    t = re.sub(r'<a href=".*".*>(.*)</a>', r'\1', texto)
-    t = re.sub(r'<.*>', ' ', t)
-    return html.unescape(t)
+from unidecode import unidecode
 
 def obterNoticias(links, out):
     for link in links:
         print('Link = ' + link + '\n')
 
         link_content = requests.get(link)
-        titulo = re.findall(r'<title>(.*)\s*.*</title>', link_content.text)
+        titulo = re.findall(r'<title>(.*[^\s\n\t\r])\s+\|.*\| PÚBLICO</title>', link_content.text)
         description = re.findall(r'<div class="story__blurb lead" itemprop="description">\s*<p>(.*)</p>\s*</div>', link_content.text)
         images = re.findall(r'data-media-viewer="(.*)"', link_content.text)
 
@@ -24,25 +20,26 @@ def obterNoticias(links, out):
         paragrafos = paragrafos + re.findall(r'<p>\s*(.*)\s*</p>.*\s*.*<aside class=".*">', link_content.text)
 
         
-        tex_folder = ''.join(titulo[0].split(' ')).lower()
+        tex_folder = ".files/" + ''.join(unidecode(titulo[0]).split(' ')).lower()
+        tex_folder = re.sub(r'"',r'',tex_folder)
         os.makedirs(tex_folder, exist_ok=True)
 
         print(titulo[0] + '\n')
-        print(limparTexto(description[0]) + '\n')
+        print(latex_utils.limparTexto(description[0]) + '\n')
         img_counter=0
         img_paths=[]
         for image in images:
-            with open(tex_folder+"/img_"+str(img_counter), "wb") as im_dump:
+            with open(tex_folder+"/img_"+str(img_counter)+".jpeg", "wb") as im_dump:
                 r = requests.get(image)
                 for chunk in r.iter_content(chunk_size=128):
                     im_dump.write(chunk)
-            img_paths.append("/img_"+str(img_counter))
+            img_paths.append("/img_"+str(img_counter)+".jpeg")
             img_counter+=1
         img_paths=[tex_folder+img_name for img_name in img_paths]
 
         for paragrafo in paragrafos:
-            print(limparTexto(paragrafo))
-            print("\n---------------------------------\n")
+            print(latex_utils.limparTexto(paragrafo))
+        print("\n---------------------------------\n")
         latex_utils.escreverLatex(out, titulo[0], description[0], img_paths, paragrafos)
 
 
@@ -60,7 +57,7 @@ def main():
         f_descriptor = open("noticias_"+argument+".tex", "w")
         latex_utils.iniciarLatex(f_descriptor)
         pesquisarNoticias(argument, f_descriptor)
-        f_descriptor.write("\n\n\\end{document}\n")
+        f_descriptor.write("\n\\end{document}")
         f_descriptor.close()
 
 if __name__=="__main__":
