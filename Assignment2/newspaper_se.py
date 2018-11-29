047ad0e319eed935c0bb082adf8a01d36e235b06
MQuaresma <miguelquaresma97@gmail.com>
2018-11-29 16:40:35 +0000

    Interactive mode implemented

--- Assignment2/newspaper_se.py
+++ Assignment2/newspaper_se.py
@@ -5,6 +5,7 @@ import getopt
 import expresso
 import publico
 import latex_utils
+import fileinput
 
 def print_usage(util_name):
     print("USAGE\n")
@@ -12,8 +13,15 @@ def print_usage(util_name):
     print("DESCRIPTION\n")
     print("-e \t Search for news in Expresso")
     print("-p \t Search for news in Publico")
-    print("-h \t Print usage/help")
+    print("-h \t Print this menu")
     print("-i \t Interactive mode")
+
+def interactive_usage():
+    print("\n\nUsage")
+    print("p [search_term ....] : search for news in Publico")
+    print("e [search_term ....] : search for news in Expresso")
+    print("q : quit")
+    print("h or help : print this menu\n\n")
 
 def scrape(terms, newspaper):
     for argument in terms:
@@ -21,7 +29,8 @@ def scrape(terms, newspaper):
         f_descriptor = open("noticias_"+ newspaper_topic + ".tex", "w")
         latex_utils.iniciarLatex(f_descriptor)
         if(newspaper == "expresso"):
-            expresso.pesquisarNoticias(argument, f_descriptor)
+            raise("Not implemented!")
+            #expresso.pesquisarNoticias(argument, f_descriptor)
         else:
             publico.pesquisarNoticias(argument, f_descriptor)
         f_descriptor.write("\n\\end{document}")
@@ -29,8 +38,27 @@ def scrape(terms, newspaper):
         os.system("pdflatex noticias_" + newspaper_topic + ".tex > /dev/null")
         print("Notícias do " + newspaper + " sobre " + argument + " podem ser encontradas no documento noticias_" + newspaper_topic + ".pdf")
 
+def interactive_mode():
+    interactive_usage()
+
+    line = ""
+    while line != "q":
+        line = input()
+        if line in ["h", "help"]:
+            interactive_usage()
+        elif line == "q":
+            pass
+        else:
+            line = line.split(' ')
+            if line[0] == "e":
+                scrape(line[1:], "expresso")
+            elif line[0] == "p":
+                scrape(line[1:], "publico")
+            else:
+                print("Invalid command! Enter h for usage")
+
 def main():
-    args, remainder = getopt.getopt(sys.argv[1:], "ehp")
+    args, remainder = getopt.getopt(sys.argv[1:], "ehip")
     args = dict(args)
     if "-h" in args:
         print_usage(sys.argv[0])
@@ -38,6 +66,8 @@ def main():
         scrape(remainder, "publico")
     if "-e" in args:
         scrape(remainder, "expresso")
+    if "-i" in args:
+        interactive_mode()
 
 if __name__ == "__main__":
     main()
